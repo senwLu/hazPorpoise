7ecc600e8c9f7e8d8655c22a595ae10d3f941f39
senwLu <swei.lu1018@gmail.com>
2019-05-04 17:40:34 -0700

    pushing initial app setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          
+      };
+      
+  };
+
+
+  render() {
+      return (
+      
+        <div>
+            <h1>Hello World</h1>
+        </div>
+        
+      )
+  };
+};
+
+export default Checkout;
